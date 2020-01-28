c3512b7c6663c008e501c728a3f4f71ee183c181
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2020-01-28 15:00:19 +0000

    New translations turtle-race-finished-turtle-race.py (Turkish)

--- tr-TR/resources/turtle-race-finished-turtle-race.py
+++ tr-TR/resources/turtle-race-finished-turtle-race.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-# translated by Volkan ÇEVİK into Turkish
 
 from turtle import *
 from random import randint
@@ -8,10 +7,10 @@ speed(0)
 penup()
 goto(-140, 140)
 
-for kademe in range(15):
-  write(kademe, align='center')
+for step in range(15):
+  write(step, align='center')
   right(90)
-  for rakam in range(8):
+  for num in range(8):
     penup()
     forward(10)
     pendown()
@@ -21,53 +20,53 @@ for kademe in range(15):
   left(90)
   forward(20)
 
-ipek = Turtle()
-ipek.color('red')
-ipek.shape('turtle')
+red = Turtle()
+red.color('red')
+red.shape('turtle')
 
-ipek.penup()
-ipek.goto(-160, 100)
-ipek.pendown()
+red.penup()
+red.goto(-160, 100)
+red.pendown()
 
-for sira in range(10):
-  ipek.right(36)
+for turn in range(10):
+  red.right(36)
 
-ceyda = Turtle()
-ceyda.color('blue')
-ceyda.shape('turtle')
+blue = Turtle()
+blue.color('blue')
+blue.shape('turtle')
 
-ceyda.penup()
-ceyda.goto(-160, 70)
-ceyda.pendown()
+blue.penup()
+blue.goto(-160, 70)
+blue.pendown()
 
-for sira in range(72):
-  ceyda.left(5)
+for turn in range(72):
+  blue.left(5)
 
-volkan = Turtle()
-volkan.shape('turtle')
-volkan.color('yellow')
+green = Turtle()
+green.shape('turtle')
+green.color('yellow')
 
-volkan.penup()
-volkan.goto(-160, 40)
-volkan.pendown()
+green.penup()
+green.goto(-160, 40)
+green.pendown()
 
-for sira in range(60):
-  volkan.right(6)
+for turn in range(60):
+  green.right(6)
 
-gulsen = Turtle()
-gulsen.shape('turtle')
-gulsen.color('turquoise')
+yellow = Turtle()
+yellow.shape('turtle')
+yellow.color('turquoise')
 
-gulsen.penup()
-gulsen.goto(-160, 10)
-gulsen.pendown()
+yellow.penup()
+yellow.goto(-160, 10)
+yellow.pendown()
 
-for sira in range(30):
-  gulsen.left(12)
+for turn in range(30):
+  yellow.left(12)
 
-for sira in range(100):
-  ipek.forward(randint(1,5))
-  ceyda.forward(randint(1,5))
-  volkan.forward(randint(1,5))
-  gulsen.forward(randint(1,5))
+for turn in range(100):
+  red.forward(randint(1,5))
+  blue.forward(randint(1,5))
+  green.forward(randint(1,5))
+  yellow.forward(randint(1,5))
   
