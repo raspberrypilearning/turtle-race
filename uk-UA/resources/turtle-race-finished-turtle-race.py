df6ae3e595dac05eaf82243581cebaa34f205b06
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-04-05 19:55:02 +0100

    New translations turtle-race-finished-turtle-race.py (Ukrainian)

--- uk-UA/resources/turtle-race-finished-turtle-race.py
+++ uk-UA/resources/turtle-race-finished-turtle-race.py
@@ -7,8 +7,8 @@ speed(0)
 penup()
 goto(-140, 140)
 
-for step in range(15):
-  write(step, align='center')
+for krok in range(15):
+  write(krok, align='center')
   right(90)
   for num in range(8):
     penup()
@@ -28,7 +28,7 @@ red.penup()
 red.goto(-160, 100)
 red.pendown()
 
-for turn in range(10):
+for krok in range(10):
   red.right(36)
 
 blue = Turtle()
@@ -39,32 +39,32 @@ blue.penup()
 blue.goto(-160, 70)
 blue.pendown()
 
-for turn in range(72):
+for krok in range(72):
   blue.left(5)
 
 green = Turtle()
 green.shape('turtle')
-green.color('yellow')
+green.color('turquoise')
 
 green.penup()
 green.goto(-160, 40)
 green.pendown()
 
-for turn in range(60):
+for krok in range(60):
   green.right(6)
 
 yellow = Turtle()
 yellow.shape('turtle')
-yellow.color('turquoise')
+yellow.color('yellow')
 
 yellow.penup()
 yellow.goto(-160, 10)
 yellow.pendown()
 
-for turn in range(30):
+for krok in range(30):
   yellow.left(12)
 
-for turn in range(100):
+for hid in range(100):
   red.forward(randint(1,5))
   blue.forward(randint(1,5))
   green.forward(randint(1,5))
